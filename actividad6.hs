6841e57326378d80e292fdde85c8408adba26497
joasasso <joasasso@gmail.com>
2025-04-25 19:00:11 -0300

    Correccion errores actividades 6 y 7

--- actividad6.hs
+++ actividad6.hs
@@ -1,3 +1,2 @@
 listas = [[1,2,3],[4,5],[10,20,30,40,50,60,70]]
-
-resultado = maximum(map length listas)
+resultado = maximum[length x| x<-listas]
--- actividad7.hs
+++ actividad7.hs
@@ -1,3 +1,2 @@
 lista = [1,2,3,4,5,6]
-
 cubos = [x^3|x<-lista]
